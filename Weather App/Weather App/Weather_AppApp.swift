265fd7d0f2ccf1a60f75856da5eeb0c69012e01c
Deepak <53313368+deepakananth79@users.noreply.github.com>
2023-05-27 17:42:39 -0400

    Add files via upload

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Deepak Ananth on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
